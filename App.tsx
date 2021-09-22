beab6baa30dc850d0686186e542a73b38d0357fd
Chinatsu Ouchi <natsu.aid@icloud.com>
2021-09-22 21:58:55 +0900

    げんちが大好きということ

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>げんち、大好き！！</Text>
       <StatusBar style="auto" />
     </View>
   );
